1eb472c0d5aada4ee3647ed818969b4f2a4ee052
LunyHerz <ptiendung28@gmail.com>
2026-08-16 23:05:45 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is Phan Tien Dung.");
+        Console.WriteLine("I am learning C# programming.");
+        Console.WriteLine("This is my first C# project.");
+    }
+}
